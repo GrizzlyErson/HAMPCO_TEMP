c41b5c11524e564403e5312260867358830dde68
Jefferson Asejo <asejo.32@gmail.com>
2025-11-27 13:43:24 +0800

    feat: Remove shopping cart icon from admin dashboard
    Removed the redundant shopping cart icon from the top section of
    dmin/admin_dashboard.php as its function was unclear and not
    contributing to the dashboard's purpose.

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -48,7 +48,7 @@ require_once "components/header.php";
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Admin Dashboard</h1>
-                        <i class="fa-solid fa-cart-plus"></i>
+
                         <!-- Notification Bell Icon -->
                     <button class="relative focus:outline-none" title="Notifications">
                         <svg class="w-8 h-8 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
