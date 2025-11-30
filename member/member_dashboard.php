c0ddade43ba866503f220bf206b61a21f07c4f6f
Jefferson Asejo <asejo.32@gmail.com>
2025-12-01 02:46:32 +0800

    Revert "FEAT: Remove redundant dropdown items from Task Progress card header"
    This reverts commit 26a69073e1d144f9c3bf816980b1cfb6f5d03785.

--- member/member_dashboard.php
+++ member/member_dashboard.php
@@ -187,7 +187,11 @@ try {
                                         </a>
                                         <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
                                             aria-labelledby="dropdownMenuLink">
-                                            <!-- No items for now -->
+                                            <div class="dropdown-header">Dropdown Header:</div>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Something else here</a>
                                         </div>
                                     </div>
                                 </div>
