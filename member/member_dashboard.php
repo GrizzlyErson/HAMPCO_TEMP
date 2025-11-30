d1bf7b2adf61f8cad44caba39248c055835620ee
Jefferson Asejo <asejo.32@gmail.com>
2025-12-01 02:31:56 +0800

    Revert "FEAT: Remove top indicators and optimize dashboard layout"
    This reverts commit 0ade317170267bfecf9a6d8326600e2f004bbc55.

--- member/member_dashboard.php
+++ member/member_dashboard.php
@@ -44,13 +44,71 @@ try {
                         </div>
                     </div>
 
+                    <!-- Content Row -->
+                    <div class="row">
+
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col-xl-4 col-md-6 mb-4">
+                            <a href="production.php?tab=assigned&status=pending" class="card border-left-warning shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-uppercase mb-1">
+                                                Pending Tasks</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800" id="pendingTasksCount">0</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clock fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col-xl-4 col-md-6 mb-4">
+                            <a href="production.php?tab=assigned&status=in_progress" class="card border-left-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-uppercase mb-1">
+                                                In Progress</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800" id="inProgressTasksCount">0</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-spinner fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <!-- Pending Requests Card Example -->
+                        <div class="col-xl-4 col-md-12 mb-4">
+                            <a href="production.php?tab=assigned&status=completed" class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-uppercase mb-1">
+                                                Completed Tasks</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800" id="completedTasksCount">0</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-check fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                     
+
                     <!-- Content Row -->
 
                     <div class="row">
 
                         <!-- Area Chart -->
-                        <div class="col-xl-8 col-lg-8">
+                        <div class="col-xl-8 col-lg-7">
                             <div class="card shadow mb-4" style="max-height: 420px;">
                                 <!-- Card Header - Dropdown -->
                                 <div
@@ -93,7 +151,7 @@ try {
                         
 
                         <!-- Pie Chart -->
-                        <div class="col-xl-4 col-lg-4">
+                        <div class="col-xl-4 col-lg-5">
                             <div class="card shadow mb-4" >
                                 <!-- Card Header - Dropdown -->
                                 <div
